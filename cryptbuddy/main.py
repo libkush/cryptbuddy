a8eb808d5d19e2766c0e0baa7f8f6838dca8617f
Kush Patel <kush@kush.in>
2023-06-04 00:58:14 +0530

    removed todos

--- cryptbuddy/main.py
+++ cryptbuddy/main.py
@@ -1,9 +1,4 @@
 from cryptbuddy.commands.core import app
 
-# TODO: Add a command to edit details of the user
-
-# TODO: Add a command to clean up everything
-
-# TODO: whenever there is an error, clean up files before exiting
 if __name__ == "__main__":
     app()
